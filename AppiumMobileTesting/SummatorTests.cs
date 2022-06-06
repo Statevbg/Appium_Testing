0cfe4f5f6da782a7b815d42852d2e281c9e533b0
Dimitar Statev <75937149+Statevbg@users.noreply.github.com>
2022-06-06 19:52:35 +0300

    Update SummatorTests.cs

--- AppiumMobileTesting/SummatorTests.cs
+++ AppiumMobileTesting/SummatorTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using System;
@@ -29,64 +29,64 @@ namespace AppiumMobileTesting
         public void Test_Two_PossitiveIntegers()
         {
             //Аrrange
-            var field1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
-            var field2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
-            var field3 = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
+            var number1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
+            var number2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
+            var calcButton = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
             //Act
-            field1.SendKeys("15");
-            field2.SendKeys("10");
-            field3.Click();
-            var field4 = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
+            number1.SendKeys("15");
+            number2.SendKeys("10");
+            calcButton.Click();
+            var result = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
             //Assert
-            Assert.That(field4.Text, Is.EqualTo("25"));
+            Assert.That(result.Text, Is.EqualTo("25"));
         }
         [Test]
         public void Test_Two_NegativeIntegers()
         {
             //Аrrange
-            var field1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
-            var field2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
-            var field3 = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
+            var number1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
+            var number2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
+            var calcButton = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
             //Act
-            field1.SendKeys("-15");
-            field2.SendKeys("-10");
-            field3.Click();
-            var field4 = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
+            number1.SendKeys("-15");
+            number2.SendKeys("-10");
+            calcButton.Click();
+            var result = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
             //Assert
-            Assert.That(field4.Text, Is.EqualTo("-25"));
+            Assert.That(result.Text, Is.EqualTo("-25"));
         }
         [TestCase("", "", "error")]
         [TestCase("12", "$%", "error")]
-        public void Test_InvalidInput(string num1, string num2, string result)
+        public void Test_InvalidInput(string num1, string num2, string expectedResult)
         {
             //Аrrange
-            var field1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
-            var field2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
-            var field3 = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
+            var number1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
+            var number2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
+            var calcButton = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
             //Act
-            field1.SendKeys(num1);
-            field2.SendKeys(num2);
-            field3.Click();
-            var field4 = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
+            number1.SendKeys(num1);
+            number2.SendKeys(num2);
+            calcButton.Click();
+            var result = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
             //Assert
-            Assert.That(field4.Text, Is.EqualTo(result));
+            Assert.That(result.Text, Is.EqualTo(expectedResult));
         }
         [TestCase("12.4", "19.3", "31.7")]
         [TestCase("-16.8", "16.8", "0.0")]
         [TestCase("-16.8", "-3.2", "-20.0")]
-        public void Test_PossitiveAndNegativeDecimals(string num1, string num2, string result)
+        public void Test_PossitiveAndNegativeDecimals(string num1, string num2, string expectedResult)
         {
             //Аrrange
-            var field1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
-            var field2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
-            var field3 = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
+            var number1 = driver.FindElementById("com.example.androidappsummator:id/editText1");
+            var number2 = driver.FindElementById("com.example.androidappsummator:id/editText2");
+            var calcButton = driver.FindElementById("com.example.androidappsummator:id/buttonCalcSum");
             //Act
-            field1.SendKeys(num1);
-            field2.SendKeys(num2);
-            field3.Click();
+            number1.SendKeys(num1);
+            number2.SendKeys(num2);
+            calcButton.Click();
             var field4 = driver.FindElementById("com.example.androidappsummator:id/editTextSum");
             //Assert
-            Assert.That(field4.Text, Is.EqualTo(result));
+            Assert.That(field4.Text, Is.EqualTo(expectedResult));
         }
     }
 }
